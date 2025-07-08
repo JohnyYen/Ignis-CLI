290d80e1a00a3054ef41a348c7cf0dfacc3cfe41
Johny <jhonnyantonio892@gmail.com>
2025-07-08 18:28:00 -0400

    feat/list-command

--- bin/ignis.js
+++ bin/ignis.js
@@ -1,11 +1,13 @@
-import { program } from 'commander';
-import loadCommands from '../utils/commander-loader';
-import createCommand from '../commands/create';
-import helpCommand from '../commands/help';
-import listCommand from '../commands/list';
+const { program } = require('commander')
+const listCommand = require('../commands/list')
+const initCLI = require('../commands/init')
+//const initCommand = require('../commands/init')
 
-createCommand(program)
-helpCommand(program)
+
+initCLI()
+
+//initCommand(program)
+//helpCommand(program)
 listCommand(program)
 
 if (!process.argv.slice(2).length) {
--- commands/list.js
+++ commands/list.js
@@ -1,21 +1,55 @@
-import fs from 'fs-extra';
-import path from 'path';
-import { fileURLToPath } from 'url';
+const fs = require('fs-extra')
+const path = require('path')
+const {fileURLToPath} = require("url")
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+//const __filename = fileURLToPath(import.meta.url);
+//const __dirname = path.dirname(__filename);
 
-export default async function listCommand(program) {
+async function listCommand(program) {
   const frameworksPath = path.resolve(__dirname, '../config/frameworks.json');
 
   program
     .command('list')
+    .option('-t, --template', "Lista además todos las plantillas por framework")
+    .option('-f, --framework <name>', "Lista todos los templates de un framework en específico")
     .description('Lista todos los frameworks soportados')
-    .action(async () => {
-      const frameworks = JSON.parse(await fs.readFile(frameworksPath, 'utf8'));
-      console.log("Frameworks soportados:");
-      for (const [key, value] of Object.entries(frameworks)) {
-        console.log(` - ${key}: ${value.name}`);
-      }
+    .action(async (options) => {
+        const frameworks = JSON.parse(await fs.readFile(frameworksPath, 'utf8'));
+        const {framework, template} = options;
+
+        if (template && !framework){
+            console.log("Plantillas por framework: ");
+
+            for(const [key, value] of Object.entries(frameworks)) {
+                console.log(`- ${value.name}`)
+                for(const [_, templ] of Object.entries(value.templates)) {
+                    console.log(`* ${templ.id}   ${templ.description}`);
+                }
+            }
+        }
+        else if(template && framework){
+            let find = false;
+
+            for(const [_, value] of Object.entries(frameworks)) {
+                if( value.name === framework){
+                    console.log(`Plantillas para ${value.name}: `);
+                    find = true
+                    for(const [_, templ] of Object.entries(value.templates))
+                        console.log(`* ${templ.id}   ${templ.description}`);
+                }
+
+            }
+
+            if(!find)
+                console.log("El framework específicado no existe")
+        }
+        else{
+            console.log("Frameworks soportados:");
+            for (const [key, value] of Object.entries(frameworks)) {
+                console.log(` - ${value.name}`);
+            }
+        }
     });
 }
+
+module.exports = listCommand
